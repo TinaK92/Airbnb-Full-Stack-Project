3893a74e45e18a23410a1db09b25f9eb7e8cc27a
TinaK92 <tinakonopko@gmail.com>
2024-10-11 16:33:48 -0400

    deploy phase completed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,35 +1,9 @@
-// create an Express router, create a test route, and export the 
-// router at the bottom of the file:
 // backend/routes/index.js
-
 const express = require('express');
 const router = express.Router();
-
-// backend/routes/index.js
-// Add a XSRF-TOKEN cookie
-// router.get("/api/csrf/restore", (req, res) => {
-  
-//     const csrfToken = req.csrfToken();
-//     res.cookie("XSRF-TOKEN", csrfToken);
-//     res.status(200).json({
-//       'XSRF-Token': csrfToken
-//     });
-//   });
-
-  if (process.env.NODE_ENV !== 'production') {
-    router.get('/api/csrf/restore', (req, res) => {
-      res.cookie('XSRF-TOKEN', req.csrfToken());
-      return res.json({});
-    });
-  }
-
-  // backend/routes/index.js
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
-
-// backend/routes/index.js
-// ... after `router.use('/api', apiRouter);`
 
 // Static routes
 // Serve React build files in production
@@ -38,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
     );
   });
@@ -49,9 +23,20 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
     );
+  });
+}
+
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+  router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
   });
 }
 
