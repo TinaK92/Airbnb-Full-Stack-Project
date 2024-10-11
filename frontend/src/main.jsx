c1f84c35b03c3a3fc9037bd75fef621d22886d06
TinaK92 <tinakonopko@gmail.com>
2024-10-11 14:02:19 -0400

    phase 1 completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import configureStore from './store';
+// import configureStore from './store';
+import configureStore from './store/store.js';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
+
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
 }
