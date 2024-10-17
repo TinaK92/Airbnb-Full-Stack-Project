dac4690198bcef40048192953fa7be6c7ae61630
TinaK92 <tinakonopko@gmail.com>
2024-10-17 15:48:02 -0400

    seed data

--- backend/db/seeders/20240923150829-Spot-Images.js
+++ backend/db/seeders/20240923150829-Spot-Images.js
@@ -8,29 +8,256 @@ if (process.env.NODE_ENV === 'production') {
 }
 const demoSpotImages =
 [ 
-  { spotId: 1, images: [
-      { url: "livingroom1.png", preview: true },
-      { url: "kitchen1.png" },
-      { url: "diningroom1.png" }
-  ]},
-  { spotId: 2, images: [
-      { url: "bathroom1.png", preview: true },
-      { url: "bedroom1.png" },
-      { url: "garden1.png" }
-  ]},
-  { spotId: 3, images: [
-      { url: "kitchen2.png", preview: true },
-      { url: "patio1.png" }
-  ]},
-  { spotId: 4, images: [
-      { url: "livingroom2.png", preview: true },
-      { url: "balcony1.png" },
-      { url: "office1.png" },
-      { url: "garage1.png" }
-  ]},
-  { spotId: 5, images: [
-      { url: "bedroom2.png", preview: true }
-  ]}
+  {
+    spotId: 1,
+    url: 'frontend/Images/House1/house1.png',
+    preview: false,
+  },
+  {
+    spotId: 1,
+    url: 'frontend/Images/House1/bedroom 4.png',
+    preview: false,
+  },
+  {
+    spotId: 1,
+    url: 'frontend/Images/House1/kitchen 12.png',
+    preview: true,
+  },
+  {
+    spotId: 1,
+    url: 'frontend/Images/House1/Dinning room.png',
+    preview: false,
+  },
+  {
+    spotId: 1,
+    url: 'frontend/Images/House1/bathroom 2.png',
+    preview: false,
+  },
+  {
+    spotId: 2,
+    url: 'frontend/Images/House2/bathroom 1.png',
+    preview: false,
+  },
+  {
+    spotId: 2,
+    url: 'frontend/Images/House2/House2.jpg',
+    preview: true,
+  },
+  {
+    spotId: 2,
+    url: 'frontend/Images/House2/kids room.png',
+    preview: false,
+  },
+  {
+    spotId: 2,
+    url: 'frontend/Images/House2/kitchen 9.png',
+    preview: false,
+  },
+  {
+    spotId: 2,
+    url: 'frontend/Images/House2/laundry.png',
+    preview: false,
+  },
+  {
+    spotId: 3,
+    url: 'frontend/Images/House3/bathrooom 6.png',
+    preview: false,
+  },
+  {
+    spotId: 3,
+    url: 'frontend/Images/House3/Bedroom.png',
+    preview: false,
+  },
+  {
+    spotId: 3,
+    url: 'frontend/Images/House3/dinning room 5.png',
+    preview: true,
+  },
+  {
+    spotId: 3, 
+    url: 'frontend/Images/House3/House3.png',
+    preview: false,
+  },
+  {
+    spotId: 3,
+    url: 'frontend/Images/House3/kitchen 7.png',
+    preview: false,
+  },
+  {
+    spotId: 4,
+    url: 'frontend/Images/House4/bathroom.png',
+    preview: false,
+  },
+  {
+    spotId: 4,
+    url: 'frontend/Images/House4/bedroom 3.png',
+    preview: false,
+  },
+  {
+    spotId: 4,
+    url: 'frontend/Images/House4/kitchen 14.png',
+    preview: false,
+  },
+  {
+    spotId: 4,
+    url: 'frontend/Images/House4/livingroom 2.png',
+    preview: true,
+  },
+  {
+    spotId: 4,
+    url: 'frontend/Images/House4/office.png',
+    preview: false,
+  },
+  {
+    spotId: 5,
+    url: 'frontend/Images/House5/bedroom.png', 
+    preview: false,
+  },
+  {
+    spotId: 5,
+    url: 'frontend/Images/House5/closet.png',
+    preview: false,
+  },
+  {
+    spotId: 5,
+    url: 'frontend/Images/House5/dinning room 4.png',
+    preview: false,
+  },
+  {
+    spotId: 5,
+    url: 'frontend/Images/House5/kitchen 15.png',
+    preview: true,
+  },
+  {
+    spotId: 5,
+    url: 'frontend/Images/House5/livingroom6.png',
+    preview: false,
+  },
+  {
+    spotId: 6,
+    url: 'frontend/Images/House6/bedroom 2.png',
+    preview: false,
+  },
+  {
+    spotId: 6,
+    url: 'frontend/Images/House6/closet.png',
+    preview: false,
+  },
+  {
+    spotId: 6,
+    url: 'frontend/Images/House6/kitchen 6.png',
+    preview: false,
+  },
+  {
+    spotId: 6,
+    url: 'frontend/Images/House6/library.png',
+    preview: true,
+  },
+  {
+    spotId: 6,
+    url: 'frontend/Images/House6/livingroom 11.png',
+    preview: false,
+  },
+  {
+    spotId: 7,
+    url: 'frontend/Images/House7/bathroom.png',
+    preview: false,
+  },
+  {
+    spotId: 7,
+    url: 'frontend/Images/House7/closet 2.png',
+    preview: true,
+  },
+  {
+    spotId: 7,
+    url: 'frontend/Images/House7/Dinning room 1.png',
+    preview: false,
+  },
+  {
+    spotId: 7,
+    url: 'frontend/Images/House7/kitchen 11.png',
+    preview: false,
+  },
+  {
+    spotId: 7,
+    url: 'frontend/Images/House7/laundry.png',
+    preview: false,
+  },
+  {
+    spotId: 8,
+    url: 'frontend/Images/House8/bedroom.png',
+    preview: false,
+  },
+  {
+    spotId: 8,
+    url: 'frontend/Images/House8/kids room 3.png',
+    preview: false,
+  },
+  {
+    spotId: 8,
+    url: 'frontend/Images/House8/kitchen-dinning 1.png',
+    preview: false,
+  },
+  {
+    spotId: 8,
+    url: 'frontend/Images/House8/livingroom.png',
+    preview: true,
+  },
+  {
+    spotId: 8,
+    url: 'frontend/Images/House8/office 1.png',
+    preview: false,
+  },
+  {
+    spotId: 9,
+    url: 'frontend/Images/House9/bed.png',
+    preview: false,
+  },
+  {
+    spotId: 9,
+    url: 'frontend/Images/House9/closet.png',
+    preview: false,
+  },
+  {
+    spotId: 9,
+    url: 'frontend/Images/House9/kids room 1.png',
+    preview: true, 
+  },
+  {
+    spotId: 9,
+    url: 'frontend/Images/House9/kitcehn 4.png',
+    preview: false,
+  },
+  {
+    spotId: 9,
+    url: 'frontend/Images/House9/livingroom 1.png',
+    preview: false,
+  },
+  {
+    spotId: 10,
+    url: 'frontend/Images/House10/bar 1.png',
+    preview: false,
+  },
+  {
+    spotId: 10,
+    url: 'frontend/Images/House10/bathroom 5.png',
+    preview: false,
+  },
+  {
+    spotId: 10,
+    url: 'frontend/Images/House10/game room.png',
+    preview: false,
+  },
+  {
+    spotId: 10,
+    url: 'frontend/Images/House10/livingroom 3.png',
+    preview: false,
+  },
+  {
+    spotId: 10,
+    url: 'frontend/Images/House10/pool.png',
+    preview: true,
+  },
 ];
 
 /** @type {import('sequelize-cli').Migration} */
